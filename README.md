662073d26a9bcb76c0bf5755a422f8c132727fad
Niko Osap <niko.osap@gmail.com>
2025-12-08 13:54:19 +0100

    0.1 add txt-parser.py, text.txt and update readme.md - add text.txt containing an example of the formatting - add txt-parser.py, which opens text.txt, separates all lines and removes all empty ones - update readme.md to reflect current functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # txt-parser-date
 Parses txt-files for dates and logs their contents in a json-file for easy access
+
+## txt-parser
+
+### Description
+
+Stores all empty line indexes in a list and removes them.
+
+<!-- tbd -->
---
+++ txt-parser.py
@@ -0,0 +1,42 @@
+import json
+import re
+import datetime
+
+
+rawText = open("text.txt", "r")
+
+splitText = rawText.read().split("\n")
+
+
+
+# active = True
+
+# while active:
+#     active = False
+#     for index in range(len(splitText)):
+#         print(index)
+#         if splitText[index]=="":
+#             active = True
+#             print(splitText[index])
+#             splitText.pop(index)
+#             break
+#         print(splitText)
+
+emptyList = []
+
+# lines start at 1, index at 0
+
+for index in range(len(splitText)-1, -1, -1):
+    if splitText[index] == "":
+        emptyList.append(index)
+
+for empty in emptyList:
+    splitText.pop(empty)
+
+print(splitText)
+
+for index in range(len(splitText)):
+    if re.match("^[0-9]{2}\.[0-9]{2}\.[0-9]{4}", splitText[index]):
+        print(splitText[index][:10])
+    else:
+        print("no date here")
