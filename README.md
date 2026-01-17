55bcdaa9c7e6a10db8040ef1b7ef68b511a1f6ae
Niko Osap <niko.osap@gmail.com>
2026-01-17 13:50:58 +0100

    0.2 add dataDict: - add dateDict as precursor of json file - update parse to save parsed date and time into 'dateDict'   - date gets saved as a key and another dictionary as the value   - time then gets saved into the date's dictionary by looking at the previous lines for a date format     - if no such format is found in the line before the current one, an exception is thrown and it goes to a while loop, trying to find the date in the previous 100 lines. In case of the index goes negative, and exception is thrown - add 'dateJson' to preview the json file in python

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Parses txt-files for dates and logs their contents in a json-file for easy acces
 
 ### Description
 
-Stores all empty line indexes in a list and removes them.
+Opens `text.txt` file, splits it along `\n` and removes all empty list items.
+Then parses for date and time formats. Saves the date inta **dictionary** which then itself becomes a dictionary further containing the **time**, whose value is the text coming *after* said time
+
+For the time to be inserted into a date's dictionary, the previous 100 lines are checked for a date format. If no such format is found or an **IndexError** occurs, the while-loop is exited and an error message is written into the console
 
 <!-- tbd -->
--- txt-parser.py
+++ txt-parser.py
@@ -19,12 +19,42 @@ for index in range(len(splitText)-1, -1, -1):
 for empty in emptyList:
     splitText.pop(empty)
 
-print(splitText)
+# print(splitText)
+
+dateDict = {}
 
 for index in range(len(splitText)):
     if re.match("^[0-9]{2}\.[0-9]{2}\.[0-9]{4}", splitText[index]):
-        print(splitText[index][:10])
+        tempDate = splitText[index]
+        dateDict[tempDate] = {}
     elif re.match("^[0-9]{2}:[0-9]{2}\.", splitText[index]):
-        print(splitText[index][:5])
+        try:
+            # takes previous line to check for a date to insert the current line's time into
+            dateDict[splitText[index-1][:10]][splitText[index][:5]] = splitText[index][6:]
+        except IndexError:
+            print(f"ERROR: Index out of bounds, line {index}")
+        except:
+            reverseIndexCount = 2
+            while True:
+                try:
+                    if re.match("^[0-9]{2}\.[0-9]{2}\.[0-9]{4}", splitText[index-reverseIndexCount]):
+                        dateDict[splitText[index-reverseIndexCount][:10]][splitText[index][:5]] = splitText[index][6:]
+                        break
+                    reverseIndexCount+=1
+                except IndexError:
+                    print(f"ERROR: Index out of bounds, line {index}")
+                    break
+                except:
+                    print(f"Unknown Error between line {index} and {index-reverseIndexCount}")
+                    break
+                if reverseIndexCount >= 100:
+                    print(f"ERROR: too many lines parsed caused by inproper formatting of date. Start of issue {index}")
+                    break
+
     else:
-        print("no date here")
+        continue
+        # print("no date here")
+
+dateJson = json.dumps(dateDict)
+
+print(dateJson)
