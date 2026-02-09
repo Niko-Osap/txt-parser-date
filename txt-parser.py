97de0057081c41da8b125723badc0f68179975de
Niko Osap <niko.osap@gmail.com>
2026-02-09 13:31:57 +0100

    0.5 add function for ease of use and consistency - (based on suggestions from @doppera-at ) - update spelling - update incorrect value set in line (now) 53

--- txt-parser.py
+++ txt-parser.py
@@ -3,9 +3,17 @@ import re
 import datetime
 
 
-rawText = open("text.txt", "r")
+def convDateString(dateLine: str):
+    dateLine = dateLine.strip("[]")
+    return dateLine[0:2] + "/" + dateLine[3:5] + "/" + dateLine[6:10]
 
-splitText = rawText.read().split("\n")
+
+
+splitText = ""
+
+with open("text.txt", "r") as rawText:
+
+    splitText = rawText.read().split("\n")
 
 
 emptyList = []
@@ -25,15 +33,13 @@ dateDict = {}
 
 for index in range(len(splitText)):
     if re.match("^\[[0-9]{2}[\.\/][0-9]{2}[\.\/][0-9]{4}\]", splitText[index]):
-        rawDate = splitText[index].strip("[]")
-        tempDate = rawDate[0:2] + "/" + rawDate[3:5] + "/" + rawDate[6:10]
+        tempDate = convDateString(splitText[index])
         dateDict[tempDate] = {}
     elif re.match("^[0-9]{2}:[0-9]{2}\.", splitText[index]):
         try:
             # takes previous line to check for a date to insert the current line's time into
-            dateNameRaw = splitText[index-1].strip("[]")
-            tempDate = dateNameRaw[0:2] + "/" + dateNameRaw[3:5] + "/" + dateNameRaw[6:10]
-            dateDict[tempDate][splitText[index][:5]] = splitText[index][6:]
+            tempDate = convDateString(splitText[index-1])
+            dateDict[tempDate][splitText[index][:5]] = splitText[index][7:]
         except IndexError:
             print(f"ERROR: Index out of bounds, line {index}")
         except:
@@ -44,10 +50,9 @@ for index in range(len(splitText)):
                     if re.match("^\[[0-9]{2}[\.\/][0-9]{2}[\.\/][0-9]{4}\]", splitText[index-reverseIndexCount]):
                         # takes date from that line and inserts time (timeStamp) and associated text (timeText) into date dictionary of found date
                         timeStamp = splitText[index][:5]
-                        timeText = splitText[index][6:]
+                        timeText = splitText[index][7:]
                         # clean and standardize date into "12/34/5678"
-                        dateNameRaw = splitText[index-reverseIndexCount].strip("[]")
-                        tempDate = dateNameRaw[0:2] + "/" + dateNameRaw[3:5] + "/" + dateNameRaw[6:10]
+                        tempDate = convDateString(splitText[index-reverseIndexCount])
                         dateDict[tempDate][timeStamp] = timeText
                         break
                     reverseIndexCount+=1
@@ -58,7 +63,7 @@ for index in range(len(splitText)):
                     print(f"Unknown Error between line {index} and {index-reverseIndexCount}")
                     break
                 if reverseIndexCount >= 100:
-                    print(f"ERROR: too many lines parsed caused by inproper formatting of date. Start of issue {index}")
+                    print(f"ERROR: too many lines parsed caused by improper formatting of date. Start of issue {index}")
                     break
 
     else:
@@ -69,6 +74,7 @@ dateJson = json.dumps(dateDict)
 
 print(dateJson)
 
-jsonFile = open("text.json", "w")
+with open("text.json", "w") as jsonFile:
 
-jsonFile.write(dateJson)
+    jsonFile.write(dateJson)
+
